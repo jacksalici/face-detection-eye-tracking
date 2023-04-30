5ff71a829a8d4616a2b4d3946b9f5c8f9a3be87f
Giacomo Salici <giambo2000@gmail.com>
2023-04-30 19:58:02 +0200

    marking landmark

--- face_landmark.py
+++ face_landmark.py
@@ -3,21 +3,22 @@ import dlib
 from imutils import face_utils
 import cv2
 
-class GazeTracking():
+class FaceLandmarkTracking():
     def __init__(self) -> None:
-        self.landmark_predictor = dlib.shape_predictor('predictors/shape_predictor_68_face_landmarks.dat')
+        self.landmark_predictor = dlib.shape_predictor(
+            'predictors/shape_predictor_68_face_landmarks.dat')
         self.face_detector = dlib.get_frontal_face_detector()
-    
+
     def face_analysis(self, frame):
         faces = self.face_detector(frame,  1)
         poi_list = []
         for face in faces:
-            
-            shape = face_utils.shape_to_np(self.landmark_predictor(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), face))
+
+            shape = face_utils.shape_to_np(self.landmark_predictor(
+                cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), face))
             (x, y, w, h) = face_utils.rect_to_bb(face)
-            
-            
-            poi = {
+
+            poi_list.append({
                 "box": (x, y, w, h),
                 "traits": {
                     "ear_dx": shape[0],
@@ -27,20 +28,19 @@ class GazeTracking():
                     "mouth_sx": shape[54],
                     "center_eye_dx": np.round(np.mean(shape[36:42], axis=0)).astype(int),
                     "center_eye_sx": np.round(np.mean(shape[42:48], axis=0)).astype(int),
-                    
-                    },
-                "eye_corners":{
+
+                },
+                "eye_corners": {
                     "dx_out": shape[36],
-                    "dx_in": shape [39],
+                    "dx_in": shape[39],
                     "sx_out": shape[45],
                     "sx_in": shape[42],
-                    "sx_top":np.round(np.mean(shape[37:38], axis=0)).astype(int),
-                    "sx_bottom":np.round(np.mean(shape[41:42], axis=0)).astype(int),
-                    "dx_top":np.round(np.mean(shape[43:45], axis=0)).astype(int),
-                    "dx_bottom":np.round(np.mean(shape[46:47], axis=0)).astype(int),
+                    "sx_top": np.round(np.mean(shape[37:38], axis=0)).astype(int),
+                    "sx_bottom": np.round(np.mean(shape[41:42], axis=0)).astype(int),
+                    "dx_top": np.round(np.mean(shape[43:45], axis=0)).astype(int),
+                    "dx_bottom": np.round(np.mean(shape[46:47], axis=0)).astype(int),
                 }
-            }
-                        
-            poi_list.append(poi)
+            })
             
+
         return poi_list
--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from face_detection_with_haar_cascade import HaarCascade
-from gaze_tracking import GazeTracking
+from face_landmark import FaceLandmarkTracking
+from pupil_detection import PupilDetection
 import cv2
 
 
-# define a video capture object
-vid = cv2.VideoCapture(0)
-cascade = HaarCascade()
-gazeTracking = GazeTracking()
+#vid = cv2.VideoCapture(0)
+landmark_tracking = FaceLandmarkTracking()
+pupil_detection = PupilDetection()
 
-while(True):
-    # Capture the video frame by frame
-    ret, frame = vid.read()
-       
-    for face in gazeTracking.face_analysis(frame):
-        for mark in face["eye_corners"].values():
-                cv2.circle(frame, (mark[0], mark[1]), 2, (0, 255, 255), -1)
-            
+#while(True):
+    #ret, frame = vid.read()
+frame = cv2.imread("rdg.jpg")
+cv2.imshow('frame',  frame)
+cv2.waitKey(10000)
+for face in landmark_tracking.face_analysis(frame):
+    for mark in face["eye_corners"].values():
+            cv2.circle(frame, (mark[0], mark[1]), 2, (0, 255, 255), -1)
+            print(mark)
+    
     cv2.imshow('frame',  frame)
+    cv2.waitKey(10000)
     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
+    x = face["eye_corners"]["sx_out"][1]-25
+    y = face["eye_corners"]["sx_top"][0]-25
+    w = abs(face["eye_corners"]["sx_in"][1] - face["eye_corners"]["sx_out"][1])+50
+    h = abs(face["eye_corners"]["sx_bottom"][0] - face["eye_corners"]["sx_top"][0])+50
+    print(x,y,w,h)
+        
+    #cv2.circle(frame, (cx, cy), 2, (0, 255, 255), -1)
 
-vid.release()
-cv2.destroyAllWindows()
+cv2.imshow('frame',  frame[y:y+h, x:x+w])
+cv2.waitKey(10000)
+
+#if cv2.waitKey(1) & 0xFF == ord('q'):
+#    break
+
+
+#vid.release()
