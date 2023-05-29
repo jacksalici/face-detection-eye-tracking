9fe193b9482c12d6eb73292db4e913a9ebb9e335
Giacomo Salici <giambo2000@gmail.com>
2023-05-29 11:27:11 +0200

    calibration

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from components.face_detection import HaarCascade
 from components.face_landmark import FaceLandmarkTracking
 from components.pupil_detection import PupilDetection
+from components.pnp_solver import PnPSolver
+
 import cv2
+import numpy as np
 
 
 vid = cv2.VideoCapture(0)
 landmark_tracking = FaceLandmarkTracking()
-
 pupil_detection = PupilDetection()
+pnp_solver = PnPSolver()
 
 while(True):
     ret, frame = vid.read()
@@ -15,20 +18,22 @@ while(True):
     framebg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     for face in landmark_tracking.face_analysis(framebg):
-        cy = face["traits"]["center_eye_sx"][0]
-        cx = face["traits"]["center_eye_sx"][1]
-        
-        
-        w = int((abs(face["eye_corners"]["sx_in"][1] - face["eye_corners"]["sx_out"][1]) + 100 )/2)
+        image_points = np.array([
+        face.get("traits").get("nose"),
+        face.get("traits").get("chin"),
+        face.get("eye_corners").get("sx_out"),
+        face.get("eye_corners").get("dx_out"),
+        face.get("traits").get("mouth_sx"),
+        face.get("traits").get("mouth_dx"),
+    ], dtype="double")
+    
+    
+        nose_end_point2D = pnp_solver.pose(frame.shape, image_points)
             
-        cv2.circle(frame, (cy, cx), w*2, (0, 255, 255), 2)
-        
-        new_img_bg = framebg[cx-w:cx+w, cy-w:cy+w]
+        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
 
-        ncy, ncx = pupil_detection.detect_pupil(framebg[cx-w:cx+w, cy-w:cy+w])
-        
-        ncy, ncx = ncy+cy-w, ncx+cx-w
-        cv2.circle(frame, (ncy, ncx), 2, (0, 255, 255), -1)
+        cv2.line(frame, p1, p2, (255,0,0), 2)
 
 
     cv2.imshow('frame',  frame)
