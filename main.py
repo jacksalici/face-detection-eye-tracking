2942c5a59abab3a4194df46a193af122d74e5ef4
Giacomo Salici <giambo2000@gmail.com>
2023-05-31 10:58:29 +0200

    padding

--- main.py
+++ main.py
@@ -28,26 +28,44 @@ while(True):
             face.get("mouth_sx"),
             face.get("mouth_dx"),
         ], dtype="double")
-         
-
-
 
         nose_end_point2D, pitch, yaw, roll = pnp_solver.pose(
             frame.shape, image_points)
-        
-        
-        
-        cv2.rectangle(frame, (face.get("eye_sx_out")[0], face.get("eye_sx_top")[1]),  (face.get("eye_sx_in")[0], face.get("eye_sx_bottom")[1]), (255, 0, 255), 1 )
-        cv2.rectangle(frame, (face.get("eye_dx_in")[0], face.get("eye_dx_top")[1]),  (face.get("eye_dx_out")[0], face.get("eye_dx_bottom")[1]), (255, 0, 255), 1 )
 
+        eye_frame_padding = 5
+        cv2.rectangle(frame, (face.get("eye_sx_in")[0]-eye_frame_padding, 
+                              face.get("eye_sx_top")[1]-eye_frame_padding),
+                      (face.get("eye_sx_out")[0]+eye_frame_padding, 
+                       face.get("eye_sx_bottom")[1]+eye_frame_padding),
+                      (255, 0, 255), 1)
+        cv2.rectangle(frame, (face.get("eye_dx_out")[0]-eye_frame_padding, 
+                              face.get("eye_dx_top")[1]-eye_frame_padding),
+                      (face.get("eye_dx_in")[0]+eye_frame_padding, 
+                       face.get("eye_dx_bottom")[1]+eye_frame_padding),
+                      (255, 0, 255), 1)
 
-        (pupil_sx_x, pupil_sx_y) = pupil_detection.detect_pupil(framebg[face.get("eye_sx_top")[1]-50: face.get("eye_sx_bottom")[1]+50, face.get("eye_sx_in")[0]-50: face.get("eye_sx_out")[0]]+50)
-        pupil_sx_y, pupil_sx_x = face.get("eye_sx_top")[1]-50+pupil_sx_y, face.get("eye_sx_in")[0]-50 + pupil_sx_x
-        
-        
+        (pupil_sx_x, pupil_sx_y) = pupil_detection.detect_pupil(framebg[
+            face.get("eye_sx_top")[1] - eye_frame_padding: 
+                face.get("eye_sx_bottom")[1] + eye_frame_padding,
+            face.get("eye_sx_in")[0] - eye_frame_padding: 
+                face.get("eye_sx_out")[0] + eye_frame_padding])
+
+        pupil_sx_y, pupil_sx_x = face.get("eye_sx_top")[1] - eye_frame_padding +pupil_sx_y, face.get("eye_sx_in")[0]  - eye_frame_padding + pupil_sx_x
+
+        (pupil_dx_x, pupil_dx_y) = pupil_detection.detect_pupil(framebg[
+            face.get("eye_dx_top")[1]-eye_frame_padding: 
+                face.get("eye_dx_bottom")[1]+eye_frame_padding,
+            face.get("eye_dx_out")[0]-eye_frame_padding: 
+                face.get("eye_sx_in")[0]+eye_frame_padding])
+
+        pupil_dx_y, pupil_dx_x = face.get("eye_dx_top")[1] - eye_frame_padding +pupil_dx_y, face.get("eye_dx_out")[0]  - eye_frame_padding + pupil_dx_x
+
         cv2.circle(frame, (pupil_sx_x, pupil_sx_y),
-                       10, (0, 255, 255), 1)
-        
+                   10, (0, 255, 255), 1)
+
+        cv2.circle(frame, (pupil_dx_x, pupil_dx_y),
+                   10, (0, 255, 255), 1)
+
         for p in list(face.values())[1:]:
             cv2.circle(frame, (int(p[0]), int(p[1])),
                        2, (255, 255, 0), -1)
