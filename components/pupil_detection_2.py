4e4955401adff975cef3e31953b054913dad9e5e
Giacomo Salici <giambo2000@gmail.com>
2023-06-01 01:19:50 +0200

    equalization

--- components/pupil_detection_2.py
+++ components/pupil_detection_2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 BLUR_RADIUS = 5
 
@@ -7,21 +8,40 @@ class PupilDetection():
     def __init__(self) -> None:
         pass
     
-    def detect_pupil(self, eye):
+    def detect_pupil(self, eye, index):
         if len(eye.shape)>2:
             eye = cv2.cvtColor(eye, cv2.COLOR_BGR2GRAY)
         
+        #eye = np.clip(eye * 1.25, 0, 255)
+        eye = eye.astype('uint8')
+        
+        eye = cv2.equalizeHist(eye)
+        cv2.imshow("h"+str(index), eye)
+
         # Gaussian blur
         blurred_eye = cv2.GaussianBlur(eye, (BLUR_RADIUS, BLUR_RADIUS), 0)
-        # Adaptive threshold
-        threshold = cv2.adaptiveThreshold(blurred_eye, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 13, 2)
+        
+        
+        kernel = np.ones((5,5),np.uint8)
+        erosion = cv2.erode(blurred_eye,kernel,iterations = 1)
+        cv2.imshow("e"+str(index), erosion)
+
+
+        threshold = cv2.adaptiveThreshold(erosion,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+        
+        cv2.imshow("c"+str(index), threshold)
         # Contours
-        contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        largest_contour = max(contours, key=cv2.contourArea)
+        
         
         # Centroid
-        M = cv2.moments(largest_contour)
-        centroid_x = int(M["m10"] / M["m00"])
-        centroid_y = int(M["m01"] / M["m00"])
-                    
+        try:
+            contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            largest_contour = max(contours, key=cv2.contourArea)
+            M = cv2.moments(largest_contour)
+            centroid_x = int(M["m10"] / M["m00"])
+            centroid_y = int(M["m01"] / M["m00"])
+        except:
+            print('error finding moments')
+            centroid_x = None
+            centroid_y = None
         return (centroid_x, centroid_y)
