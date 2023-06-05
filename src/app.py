deaa702b896a693c74908399123acc1ababa78c7
Giacomo Salici <giambo2000@gmail.com>
2023-06-06 00:36:01 +0200

    costants for the string names

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from components.face_landmark import FaceLandmarkTracking
+from components.face_landmark import FaceLandmarkTracking, FaceLandmarkK
 from components.pupil_detection_means_gradients import PupilDetection
 from components.pupil_detection_filtering import PupilDetection as PupilDetection2
 from components.pnp_solver import PnPSolver
@@ -8,18 +8,20 @@ import numpy as np
 import os
 
 
-def tracking(frame, pupil_detection_mode):
+def tracking(frame: np.ndarray, pupil_detection_mode: str):
+
+
     framebg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     for face in landmark_tracking.face_analysis(framebg):
 
         image_points = np.array([
-            face.get("nose"),
-            face.get("chin"),
-            face.get("eye_sx_out"),
-            face.get("eye_dx_out"),
-            face.get("mouth_sx"),
-            face.get("mouth_dx"),
+            face.get(k.NOSE),
+            face.get(k.CHIN),
+            face.get(k.EYE_SX_OUT),
+            face.get(k.EYE_DX_OUT),
+            face.get(k.MOUTH_SX),
+            face.get(k.MOUTH_DX),
         ], dtype="double")
 
         nose_end_point2D, pitch, yaw, roll = pnp_solver.pose(
@@ -35,15 +37,15 @@ def tracking(frame, pupil_detection_mode):
         eye_frame_vertical_padding = 2
 
         eyes = [framebg[
-            face.get("eye_sx_top")[1] - eye_frame_vertical_padding:
-                face.get("eye_sx_bottom")[1] + eye_frame_vertical_padding,
-            face.get("eye_sx_in")[0] - eye_frame_horizontal_padding:
-                face.get("eye_sx_out")[0] + eye_frame_horizontal_padding],
+            face.get(k.EYE_SX_TOP)[1] - eye_frame_vertical_padding:
+                face.get(k.EYE_SX_BOTTOM)[1] + eye_frame_vertical_padding,
+            face.get(k.EYE_SX_IN)[0] - eye_frame_horizontal_padding:
+                face.get(k.EYE_SX_OUT)[0] + eye_frame_horizontal_padding],
                 framebg[
-            face.get("eye_dx_top")[1]-eye_frame_vertical_padding:
-                face.get("eye_dx_bottom")[1]+eye_frame_vertical_padding,
-            face.get("eye_dx_out")[0]-eye_frame_horizontal_padding:
-                face.get("eye_dx_in")[0]+eye_frame_horizontal_padding]
+            face.get(k.EYE_DX_TOP)[1]-eye_frame_vertical_padding:
+                face.get(k.EYE_DX_BOTTOM)[1]+eye_frame_vertical_padding,
+            face.get(k.EYE_DX_OUT)[0]-eye_frame_horizontal_padding:
+                face.get(k.EYE_DX_IN)[0]+eye_frame_horizontal_padding]
                 ]
 
         pupil_detection_modes = {
@@ -54,16 +56,16 @@ def tracking(frame, pupil_detection_mode):
         pupil_sx_x, pupil_sx_y = pupil_detection_modes[pupil_detection_mode].detect_pupil(eyes[0])
         pupil_dx_x, pupil_dx_y = pupil_detection_modes[pupil_detection_mode].detect_pupil(eyes[1])
 
-        pupil_sx_y, pupil_sx_x = face.get("eye_sx_top")[
-            1] - eye_frame_vertical_padding + pupil_sx_y, face.get("eye_sx_in")[0] - eye_frame_horizontal_padding + pupil_sx_x
-        pupil_dx_y, pupil_dx_x = face.get("eye_dx_top")[
-            1] - eye_frame_vertical_padding + pupil_dx_y, face.get("eye_dx_out")[0] - eye_frame_horizontal_padding + pupil_dx_x
+        pupil_sx_y, pupil_sx_x = face.get(k.EYE_SX_TOP)[
+            1] - eye_frame_vertical_padding + pupil_sx_y, face.get(k.EYE_SX_IN)[0] - eye_frame_horizontal_padding + pupil_sx_x
+        pupil_dx_y, pupil_dx_x = face.get(k.EYE_DX_TOP)[
+            1] - eye_frame_vertical_padding + pupil_dx_y, face.get(k.EYE_DX_OUT)[0] - eye_frame_horizontal_padding + pupil_dx_x
 
         # horizzontal ratio that expresses how centered the pupil is within the eyes, from -0.5 to 0.5, 0 is center.
-        pupil_sx_center_h_ratio = round((pupil_sx_x - face.get("eye_sx_in")[0]) / (
-            face.get("eye_sx_out")[0] - face.get("eye_sx_in")[0]) - 0.5, 2)
-        pupil_dx_center_h_ratio = round((pupil_dx_x - face.get("eye_dx_out")[0]) / (
-            face.get("eye_dx_in")[0] - face.get("eye_dx_out")[0]) - 0.5, 2)
+        pupil_sx_center_h_ratio = round((pupil_sx_x - face.get(k.EYE_SX_IN)[0]) / (
+            face.get(k.EYE_SX_OUT)[0] - face.get(k.EYE_SX_IN)[0]) - 0.5, 2)
+        pupil_dx_center_h_ratio = round((pupil_dx_x - face.get(k.EYE_DX_OUT)[0]) / (
+            face.get(k.EYE_DX_IN)[0] - face.get(k.EYE_DX_OUT)[0]) - 0.5, 2)
         gaze_facing = False
 
         if face_facing:
@@ -74,18 +76,18 @@ def tracking(frame, pupil_detection_mode):
             gaze_facing = True
 
         try:
-            cv2.rectangle(frame, (face.get("box")[0], face.get("box")[1]), (face.get("box")[
-                          0]+face.get("box")[2], face.get("box")[1]+face.get("box")[3]), (255, 0, 255), 2)
+            cv2.rectangle(frame, (face.get(k.BOX)[0], face.get(k.BOX)[1]), (face.get(k.BOX)[
+                          0]+face.get(k.BOX)[2], face.get(k.BOX)[1]+face.get(k.BOX)[3]), (255, 0, 255), 2)
 
-            cv2.rectangle(frame, (face.get("eye_sx_in")[0]-eye_frame_horizontal_padding,
-                                  face.get("eye_sx_top")[1]-eye_frame_vertical_padding),
-                          (face.get("eye_sx_out")[0]+eye_frame_horizontal_padding,
-                           face.get("eye_sx_bottom")[1]+eye_frame_vertical_padding),
+            cv2.rectangle(frame, (face.get(k.EYE_SX_IN)[0]-eye_frame_horizontal_padding,
+                                  face.get(k.EYE_SX_TOP)[1]-eye_frame_vertical_padding),
+                          (face.get(k.EYE_SX_OUT)[0]+eye_frame_horizontal_padding,
+                           face.get(k.EYE_SX_BOTTOM)[1]+eye_frame_vertical_padding),
                           (255, 0, 255), 2)
-            cv2.rectangle(frame, (face.get("eye_dx_out")[0]-eye_frame_horizontal_padding,
-                                  face.get("eye_dx_top")[1]-eye_frame_vertical_padding),
-                          (face.get("eye_dx_in")[0]+eye_frame_horizontal_padding,
-                           face.get("eye_dx_bottom")[1]+eye_frame_vertical_padding),
+            cv2.rectangle(frame, (face.get(k.EYE_DX_OUT)[0]-eye_frame_horizontal_padding,
+                                  face.get(k.EYE_DX_TOP)[1]-eye_frame_vertical_padding),
+                          (face.get(k.EYE_DX_IN)[0]+eye_frame_horizontal_padding,
+                           face.get(k.EYE_DX_BOTTOM)[1]+eye_frame_vertical_padding),
                           (255, 0, 255), 2)
         except:
             print("Error during info display")
@@ -101,7 +103,7 @@ def tracking(frame, pupil_detection_mode):
                 cv2.circle(frame, (int(p[0]), int(p[1])),
                            2, (255, 255, 0), -1)
 
-            cv2.putText(frame, f"Pupil horizzontal ratios: {pupil_dx_center_h_ratio}, {pupil_sx_center_h_ratio}", (face.get("eye_dx_out")[0], 160),
+            cv2.putText(frame, f"Pupil horizzontal ratios: {pupil_dx_center_h_ratio}, {pupil_sx_center_h_ratio}", (face.get(k.EYE_DX_OUT)[0], 160),
                         1, 1, (255, 255, 255), 1, cv2.LINE_AA)
         except:
             print("Error during info display")
@@ -115,29 +117,30 @@ def tracking(frame, pupil_detection_mode):
                 nose_end_point2D[2].ravel().astype(int)), (0, 0, 255), 2)
 
             if roll and pitch and yaw:
-                cv2.putText(frame, "Roll: " + str(round(roll)), (face.get("eye_dx_out")[0], 100),
+                cv2.putText(frame, "Roll: " + str(round(roll)), (face.get(k.EYE_DX_OUT)[0], 100),
                             1, 1, (255, 255, 255), 1, cv2.LINE_AA)
-                cv2.putText(frame, "Pitch: " + str(round(pitch)), (face.get("eye_dx_out")[0], 120),
+                cv2.putText(frame, "Pitch: " + str(round(pitch)), (face.get(k.EYE_DX_OUT)[0], 120),
                             1, 1, (255, 255, 255), 1, cv2.LINE_AA)
-                cv2.putText(frame, "Yaw: " + str(round(yaw)), (face.get("eye_dx_out")[0], 140),
+                cv2.putText(frame, "Yaw: " + str(round(yaw)), (face.get(k.EYE_DX_OUT)[0], 140),
                             1, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
         except:
             print("Error during info display")
 
         try:
-            cv2.putText(frame, "Face facing camera: " + str(int(face_facing)), (face.get("eye_dx_out")[0], 180),
+            cv2.putText(frame, "Face facing camera: " + str(int(face_facing)), (face.get(k.EYE_DX_OUT)[0], 180),
                         1, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
-            cv2.putText(frame, "Gaze facing camera: " + str(int(gaze_facing)), (face.get("eye_dx_out")[0], 200),
+            cv2.putText(frame, "Gaze facing camera: " + str(int(gaze_facing)), (face.get(k.EYE_DX_OUT)[0], 200),
                         1, 1, (255, 255, 255), 1, cv2.LINE_AA)
         except:
             print("Error during info display")
 
     cv2.imshow('frame',  frame)
+    return frame
 
 
-def init(video=False, pupil_detection_mode="grad_means", image_path='images/face1.png'):
+def init(video=False, pupil_detection_mode="grad_means", image_path=os.path.join('resources','images','face2.jpg')):
 
     if video:
         vid = cv2.VideoCapture(0)
@@ -152,14 +155,15 @@ def init(video=False, pupil_detection_mode="grad_means", image_path='images/face
         vid.release()
     else:
         frame = cv2.imread(image_path)
-        tracking(frame, pupil_detection_mode)
+        frame = tracking(frame, pupil_detection_mode)
+        cv2.imwrite(os.path.splitext(image_path)[0] + '_edited' + os.path.splitext(image_path)[1], frame)
         cv2.waitKey()
 
 
 landmark_tracking = FaceLandmarkTracking(os.path.join('resources','predictors','shape_predictor_68_face_landmarks.dat'))
 pupil_detection = PupilDetection()
 pupil_detection_2 = PupilDetection2()
-
+k = FaceLandmarkK()
 
 
 pnp_solver = PnPSolver()  # to use the calibration (np.load('calib_results.npz'))
--- src/components/face_landmark.py
+++ src/components/face_landmark.py
@@ -4,10 +4,31 @@ from imutils import face_utils
 import cv2
 
 
+class FaceLandmarkK():
+    BOX = 'box'
+    EAR_DX = 'ear_dx'
+    EAR_SX = 'ear_sx'
+    NOSE = 'nose'
+    MOUTH_DX = 'mouth_dx'
+    MOUTH_SX = 'mouth_sx'
+    CHIN = 'chin'
+    EYE_DX_CENTER = 'eye_dx_center'
+    EYE_SX_CENTER = 'eye_sx_center'
+    EYE_DX_OUT = 'eye_dx_out'
+    EYE_DX_IN = 'eye_dx_in'
+    EYE_SX_OUT = 'eye_sx_out'
+    EYE_SX_IN = 'eye_sx_in'
+    EYE_DX_TOP = 'eye_dx_top'
+    EYE_DX_BOTTOM = 'eye_dx_bottom'
+    EYE_SX_TOP = 'eye_sx_top'
+    EYE_SX_BOTTOM = 'eye_sx_bottom'
+
+
 class FaceLandmarkTracking():
     def __init__(self, shape_predictor_path) -> None:
         self.landmark_predictor = dlib.shape_predictor(shape_predictor_path)
         self.face_detector = dlib.get_frontal_face_detector()
+        self.k = FaceLandmarkK()
 
     def face_analysis(self, frame):
         faces = self.face_detector(frame,  1)
@@ -19,24 +40,23 @@ class FaceLandmarkTracking():
             (x, y, w, h) = face_utils.rect_to_bb(face)
 
             poi.append({
-                "box": (x, y, w, h),
-                "ear_dx": points[0],
-                "ear_sx": points[16],
-                "nose": points[30],
-                "mouth_dx": points[48],
-                "mouth_sx": points[54],
-                "chin": points[8],
-                "eye_dx_center": np.round(np.mean(points[36:42], axis=0)).astype(int),
-                "eye_sx_center": np.round(np.mean(points[42:48], axis=0)).astype(int),
-                "eye_dx_out": points[36],
-                "eye_dx_in": points[39],
-                "eye_sx_out": points[45],
-                "eye_sx_in": points[42],
-                "eye_dx_top": np.round(np.mean(points[37:39], axis=0)).astype(int),
-                "eye_dx_bottom": np.round(np.mean(points[40:42], axis=0)).astype(int),
-                "eye_sx_top": np.round(np.mean(points[43:45], axis=0)).astype(int),
-                "eye_sx_bottom": np.round(np.mean(points[46:48], axis=0)).astype(int),
+                self.k.BOX: (x, y, w, h),
+                self.k.EAR_DX: points[0],
+                self.k.EAR_SX: points[16],
+                self.k.NOSE: points[30],
+                self.k.MOUTH_DX: points[48],
+                self.k.MOUTH_SX: points[54],
+                self.k.CHIN: points[8],
+                self.k.EYE_DX_CENTER: np.round(np.mean(points[36:42], axis=0)).astype(int),
+                self.k.EYE_SX_CENTER: np.round(np.mean(points[42:48], axis=0)).astype(int),
+                self.k.EYE_DX_OUT: points[36],
+                self.k.EYE_DX_IN: points[39],
+                self.k.EYE_SX_OUT: points[45],
+                self.k.EYE_SX_IN: points[42],
+                self.k.EYE_DX_TOP: np.round(np.mean(points[37:39], axis=0)).astype(int),
+                self.k.EYE_DX_BOTTOM: np.round(np.mean(points[40:42], axis=0)).astype(int),
+                self.k.EYE_SX_TOP: np.round(np.mean(points[43:45], axis=0)).astype(int),
+                self.k.EYE_SX_BOTTOM: np.round(np.mean(points[46:48], axis=0)).astype(int),
             })
-
 
         return poi
