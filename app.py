4029724fb8c500b8382b26ea0ae06f72ee28d4de
Giacomo Salici <giambo2000@gmail.com>
2023-06-16 22:23:21 +0200

    renamed folder

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-from gaze_detection.main import GazeDetection
+from src.main import GazeDetection
 
 g=GazeDetection(print_on_serial=True, annotate_image=True, pupil_detection_mode="grad_means", serial_writing_step=7, crop_frame=False)
